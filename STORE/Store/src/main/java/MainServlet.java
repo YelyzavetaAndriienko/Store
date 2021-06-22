4614eb5b31d9421736e8ca9b72a65266218d701c
YelyzavetaAndriienko <65223500+YelyzavetaAndriienko@users.noreply.github.com>
2021-06-22 13:44:46 +0300

    Added project

---
+++ STORE/Store/src/main/java/MainServlet.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import java.util.ArrayList;
+
+import Databases.SQLiteClass;
+import org.json.JSONObject;
+
+@WebServlet("/")
+public class MainServlet extends HttpServlet {
+
+@Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        if (dispatcher != null) {
+            dispatcher.forward(request, response);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        super.doPost(request, response);
+    }
+}
